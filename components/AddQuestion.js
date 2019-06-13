df4e0866b67549576d4db0d0b3365991eac5c3d7
limitless2014 <37983571+limitless2014@users.noreply.github.com>
2019-06-13 18:07:28 +0430

    adding like and dislike button

--- components/AddQuestion.js
+++ components/AddQuestion.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View,TextInput } from 'react-native'
-import {Textarea,Button,Form,Picker} from 'native-base';
+import { Text, View,TextInput,Button } from 'react-native'
+import {Textarea,Form,Picker,Icon } from 'native-base';
 import {f} from '../firebaseConfig/config';
+
+
 export default class AddQuestion extends Component {
+  constructor(props){
+    super(props);
+  }
 
     state={
         title:'',
         subject:'',
+        author:'',
         selected: "computer and Technology",
-        category:''
+        category:'',
+        bold:false
     }
 
 
@@ -16,8 +23,10 @@ export default class AddQuestion extends Component {
         if(title !='' && subject !='')
         {
          let uid=f.auth().currentUser.uid;
+         let email=f.auth().currentUser.email;
          f.database().ref(`/Questions/${uid}`)
-         .push({title:title,comments:subject,date: new Date().getTime(),category:this.state.selected}).then((data)=>{
+         .push({title:title,likes:0,dislikes:0,comments:subject,category:this.state.selected,author:email,date: new Date().getTime()})
+         .then((data)=>{
            //success callback
            console.log('data ' , data)
            }).catch((error)=>{
@@ -31,6 +40,10 @@ export default class AddQuestion extends Component {
        
        }
 
+
+       bold=()=>{
+         this.setState({bold:!this.state.bold});
+       }
        onValueChange(value) {
         this.setState({
           selected: value
@@ -53,14 +66,16 @@ export default class AddQuestion extends Component {
                     <Picker.Item label="computer and Technology" value="computer and Technology" />
                     <Picker.Item label="Biology" value="Biology" />
                     <Picker.Item label="Music" value="Music" />
+                    <Picker.Item label="History" value="History" />
                     <Picker.Item label="Movie and Cinema" value="Movie and Cinema" />
                     <Picker.Item label="Sport" value="Sport" />
                     </Picker>
                 </Form>
                 <Textarea style={{margin:5}} rowSpan={5} onChangeText={(txt)=>this.setState({comments:txt})} placeholder="Comments"/>
-                <Button style={{alignSelf:'center',width:'20%',justifyContent:'center'}} onPress={()=>this.createQuestion(this.state.title,this.state.comments)}>
-                <Text style={{textAlign:'center'}}>Post</Text>
-                </Button>
+                <Button title="Post" style={{alignSelf:'center',width:'20%',justifyContent:'center'}}
+                 onPress={()=>this.createQuestion(this.state.title,this.state.comments)}/>
+               
+                
             </View>
         )
     }
--- components/Questions.js
+++ components/Questions.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import {f} from '../firebaseConfig/config';
 import {FlatList} from'react-native';
-
+import {Button, Icon,} from 'native-base'
 
 export default class Questions extends Component {
 
@@ -24,9 +24,9 @@ export default class Questions extends Component {
             
             const Questions = snapshot.val();
             let newData=[];
-            Object.values(Questions).forEach(function (item) {
-              newData.push({item});
             
+            Object.entries(Questions).forEach(([key, val])=> {
+              newData.push({val,key});
             });
             console.log(newData,newData);
             this.setState({data:newData});
@@ -43,24 +43,52 @@ export default class Questions extends Component {
            this.setState({refresh:false});
        }
 
+       like=(key,likes)=>{
+        let uid=f.auth().currentUser.uid;
+        let updatedlikes=likes+1;
+        f.database().ref(`Questions/${uid}/${key}/`).update({likes:updatedlikes});
+       }
+
+       dislike=(key,dislikes)=>{
+        let uid=f.auth().currentUser.uid;
+        let updateddislikes=dislikes+1;
+        f.database().ref(`Questions/${uid}/${key}/`).update({dislikes:updateddislikes});
+       }
 
 
 
     render() {
         return (
-            <View style={{flex:1}}>
+            
                 <FlatList
                     refreshing={this.state.refresh}
                     onRefresh={this.loadNew}
                     data={this.state.data}
                     keyExtractor={(item,index)=>index.toString()}
                     renderItem={({item,index}) => (
-                    <View key={index} style={{borderWidth:1}}>
-                        <Text>{item.item.title}</Text>
+                    <View key={index} style={{flex:1,flexDirection:'column',justifyContent:'flex-end',borderWidth:1,margin:5,height:150}}>
+                      <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+                      <Text style={{fontWeight:'bold',margin:5}}>Author : {item.val.author}</Text>
+                      <Text style={{margin:5}}>{new Date(item.val.date).toLocaleDateString("en-US") }</Text>
+                      <Text style={{margin:5}}>{new Date(item.val.date).toLocaleTimeString("en-US") }</Text>
+                      </View>
+                        <Text style={{margin:5}}>{item.val.title}</Text>
+                        <Text style={{margin:5}}>{item.val.comments}</Text>
+                        <View style={{flexDirection:'row'}}>
+                        <Button transparent  style={{margin:5,justifyContent:'center',marginLeft:5}} onPress={()=>this.like(item.key,item.val.likes)}>
+                        <Text>{item.val.likes}</Text>
+                          <Icon type="AntDesign" name="like2" style={{color:'green'}} />
+                        </Button>
+                        <Button transparent  style={{margin:5,justifyContent:'center',marginLeft:5}} onPress={()=>this.dislike(item.key,item.val.dislikes)} >
+                        <Text>{item.val.dislikes}</Text>
+                          <Icon type="AntDesign" name="dislike2" style={{color:'red'}} />
+                        </Button>
+                     
+                        </View>
                     </View>
                     )}
                     />
-            </View>
+           
         )
     }
 }
